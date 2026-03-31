8f17aadfb5bc81eeacc75a3c8010e0bef7e5977a
alizakhan21400-sys <alizakhan21400@gmail.com>
2026-03-31 11:54:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+subjects = int(input("Enter number of subjects: "))
 
+for i in range(subjects):
+    name = input("Enter subject name: ")
+    modules = int(input(f"Enter number of modules in {name}: "))
+    
+    print(f"{name} has {modules} modules")
+
+print("Study planner will be generated...")
